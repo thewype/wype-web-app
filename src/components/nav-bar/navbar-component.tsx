08fd2d00177420c5bf5d1c7635ef29e7cb5d5ac6
Shivaraj Padala <shivaraj@inncircles.com>
2024-06-15 08:43:57 +0530

    navbar added

--- src/components/nav-bar/navbar-component.tsx
+++ src/components/nav-bar/navbar-component.tsx
@@ -1,8 +1,34 @@
-import './navbar-component.scss';
+'use client'
 
-const Navbar = () =>{
-    return(
-        <nav></nav>
+import './navbar-component.scss';
+import Link from 'next/link';
+import wypeLogo from '../../../public/images/wype.svg';
+import Image from 'next/image';
+import { ShoppingCart, UserCircle } from '@phosphor-icons/react/dist/ssr';
+import { usePathname } from 'next/navigation';
+
+const Navbar = () => {
+    const pathname = usePathname()
+    return (
+        <nav>
+            <div className="navbar-wrapper container">
+                <div className="nav-logo">
+                    <Image src={wypeLogo} fill={true} alt='wype logo'  />
+                </div>
+                <div className='nav-links-list'>
+                    <Link className={`nav-link ${pathname === '/' ? 'nav-link-active' : ''}`} href={'/'}>HOME</Link>
+                    <Link className={`nav-link ${pathname === '/services' ? 'nav-link-active' : ''}`} href={'/services'}>SERVICES</Link>
+                    <Link className={`nav-link ${pathname === '/packages' ? 'nav-link-active' : ''}`} href={'/packages'}>PACKAGES</Link>
+                    <Link className={`nav-link ${pathname === '/about' ? 'nav-link-active' : ''}`} href={'/about'}>ABOUT US</Link>
+                    <Link className={`nav-link ${pathname === '/gallery' ? 'nav-link-active' : ''}`} href={'/gallery'}>GALLERY</Link>
+                    <Link className={`nav-link ${pathname === '/contact' ? 'nav-link-active' : ''}`} href={'/contact'}>CONTACT</Link>
+                </div>
+                <div className="nav-trailing-flex">
+                    <UserCircle />
+                    <ShoppingCart />
+                </div>
+            </div>
+        </nav>
     )
 }
 
