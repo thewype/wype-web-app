06981e4afa5f7e2edeb7c5860413ff7e63d63cab
Shivaraj Padala <shivaraj@inncircles.com>
2024-06-16 07:11:43 +0530

    nav responsive made

--- src/components/nav-bar/navbar-component.tsx
+++ src/components/nav-bar/navbar-component.tsx
@@ -4,27 +4,37 @@ import './navbar-component.scss';
 import Link from 'next/link';
 import wypeLogo from '../../../public/images/wype.svg';
 import Image from 'next/image';
-import { ShoppingCart, UserCircle } from '@phosphor-icons/react/dist/ssr';
+import { List, ShoppingCart, UserCircle, X } from '@phosphor-icons/react/dist/ssr';
 import { usePathname } from 'next/navigation';
+import WypeButton from '../button/button-component';
+import { useState } from 'react';
 
 const Navbar = () => {
-    const pathname = usePathname()
+    const pathname = usePathname();
+
+    const [isNavOpen, setNavState] = useState<boolean>(false);
+
     return (
         <nav>
             <div className="navbar-wrapper container">
-                <div className="nav-logo">
-                    <Image src={wypeLogo} fill={true} alt='wype logo'  />
-                </div>
-                <div className='nav-links-list'>
+                <List className='menu' onClick={() => setNavState(!isNavOpen)} />
+                <Link className="nav-logo" href={'/'}>
+                    <Image src={wypeLogo} fill={true} alt='wype logo' />
+                </Link>
+                <div className='nav-links-list' style={{ 'left': isNavOpen ? '0px' : '-100%' }}>
+                    <X className='menu-close' weight='bold' onClick={() => setNavState(!isNavOpen)} />
                     <Link className={`nav-link ${pathname === '/' ? 'nav-link-active' : ''}`} href={'/'}>HOME</Link>
                     <Link className={`nav-link ${pathname === '/services' ? 'nav-link-active' : ''}`} href={'/services'}>SERVICES</Link>
                     <Link className={`nav-link ${pathname === '/packages' ? 'nav-link-active' : ''}`} href={'/packages'}>PACKAGES</Link>
                     <Link className={`nav-link ${pathname === '/about' ? 'nav-link-active' : ''}`} href={'/about'}>ABOUT US</Link>
                     <Link className={`nav-link ${pathname === '/gallery' ? 'nav-link-active' : ''}`} href={'/gallery'}>GALLERY</Link>
                     <Link className={`nav-link ${pathname === '/contact' ? 'nav-link-active' : ''}`} href={'/contact'}>CONTACT</Link>
+                    <div className="login-btn-wrapper">
+                        <WypeButton buttonText='LOGIN' fontSize='16px' fontWeight='600' click={() => { }} />
+                    </div>
                 </div>
                 <div className="nav-trailing-flex">
-                    <UserCircle />
+                    <UserCircle className='user-account' />
                     <ShoppingCart />
                 </div>
             </div>
